3360fa8be28fe857c5803076e0f54edf1f4fab0f
Ivan Matveev <evanmatveev@gmail.com>
2018-08-31 00:15:03 +0300

    Hijacl the request to return html content to the page

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,6 +1,13 @@
 self.addEventListener('fetch', function(event) {
 	// TODO: respond to all requests with an html response
 	// containing an element with class="a-winner-is-me".
-	// Ensure the Content-Type of the response is "text/html"
+    // Ensure the Content-Type of the response is "text/html"
+
+    event.respondWith(
+        new Response("<div class='a-winner-is-me'>Hijacking Request</div>", {
+            headers: {"Content-Type": "text/html" }
+        })
+    )
+    
   console.log(event.request);
 });
